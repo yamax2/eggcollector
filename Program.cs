9dc161fe41995ce72f7785e2ed76093bd76b6fb9
egor19saz <129074822+egor19saz@users.noreply.github.com>
2023-05-28 23:57:02 +0500

    my changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace EggCollector
     {
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new EggCollector())
             {
                 game.Run();
             }
